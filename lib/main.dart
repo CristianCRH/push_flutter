c8c6dd3536795af17866cd4e8417e6651a10b735
crh <huayanayricardo88@gmail.com>
2019-11-25 18:35:27 -0500

    Notificacion completada

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:push_local/src/pages/home_pages.dart';
+import 'package:push_local/src/pages/mensaje_page.dart';
+import 'package:push_local/src/providers/push_notifications_provider.dart';
+
+void main() =>runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final GlobalKey<NavigatorState> navigatorKey= new GlobalKey<NavigatorState>();
+
+  @override
+  void initState(){
+    super.initState();
+
+    final pushProvider = new PushNotificationProvider();
+    pushProvider.initNotifications();
+
+    pushProvider.mesdajes.listen((argumento){
+      //Navigator.pushNamed(context, 'mensaje');
+      print('Este es el argumento');
+      //* hcaer todo *//
+      print(argumento);
+      
+      navigatorKey.currentState.pushNamed('mensaje',arguments: argumento);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      title: 'Materil App',
+      initialRoute: 'home',
+      routes: {
+        'home':(BuildContext context)=>HomePage(),
+        'mensaje': (BuildContext context)=>MessagePage(),
+      },
+    );
+  }
+}
---
+++ lib/src/providers/push_notifications_provider.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:async';
+import 'dart:io';
+class PushNotificationProvider{
+  FirebaseMessaging _fb = FirebaseMessaging();
+  final _messagesStreamController=StreamController<String>.broadcast();//capturar data
+  Stream<String> get mesdajes=>_messagesStreamController.stream;
+  initNotifications(){
+    _fb.requestNotificationPermissions();
+    _fb.getToken().then((token){
+      print('=== FMC Token ===');
+      print(token);
+      //c6yf7EqIVQ4:APA91bE9yXOrQh0fsOUBu9Q4wus_ydOOt0112RFZQ-ytbX1F8Et9qzJ4cI5VBlTXimvOYXkDf-385U8YxHI0hNro660OLHn3Zg9K3WkvTpWwVtEUVEdPQfg1K0icEpttoM4MZJ_6jeH-
+    });
+
+    _fb.configure(
+      //cuando la aplication está abierta
+      onMessage:(info){
+        print('====== ON MESSAGE ======');
+        print(info);
+
+        String argumento= 'no-data';
+        if(Platform.isAndroid){
+          argumento= info['data']['id']??'no-data';
+        }
+        _messagesStreamController.sink.add(argumento);
+      },
+      //cuando la aplicacion está terminada  
+      onLaunch: (info){
+        print('====== ON LAOUNCH ======');
+        print(info);
+
+
+      },
+      onResume:(info){
+        print('====== ON RESUME ======');
+        print(info);
+
+        final noti=info['data']['id'];
+       _messagesStreamController.sink.add(noti);
+      }
+    );
+  }
+
+  dispose(){
+    _messagesStreamController?.close();
+  }
+}
